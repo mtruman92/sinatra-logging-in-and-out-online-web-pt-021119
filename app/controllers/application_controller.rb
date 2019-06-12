d21cba3dc737c8285744afb8ba8829aa4a2dcca6
Mickae Truman <mtruman@nyit.edu>
2019-06-12 02:20:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,20 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do
-    erb :index
-  end
+  
+ post '/login' do
 
-  post '/login' do
     @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
     end
     erb :error
-
   end
 
   get '/account' do
-     @current_user = User.find_by_id(session[:user_id])
+
+    @current_user = User.find_by_id(session[:user_id])
     if @current_user
       erb :account
     else
@@ -30,7 +28,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-   session.clear
+
+    session.clear
     redirect to '/'
   end
 
