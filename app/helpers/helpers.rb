7f2e65a938fe2fcda1bd28a61aab4d30510a727e
Mickae Truman <mtruman@nyit.edu>
2019-06-12 02:16:49 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,12 @@
 class Helpers
+  
   def self.current_user(session)
-    @user = User.find_by_id(session[:user_id])
+    user = User.find(session[:user_id])
+    user
   end
 
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
+  
 end
